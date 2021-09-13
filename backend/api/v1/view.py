13b064b2b66ef73346f3c6fd6d93be907d57d134
csourabh8824 <csourabh8824@gmail.com>
2021-09-13 16:27:42 +0530

    Added Docker and updated api

--- backend/api/v1/view.py
+++ backend/api/v1/view.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from flask_restplus import Api,Resource
+from flask_restplus import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument("html_content", type=str)
 
 
 @api.route("/copycat")
 class CopyCat(Resource):
-    def get(self):
-        return {"status": "running"}
+    def html_duplicate_algorithm(self, html_data):
+        return html_data
+
+    @api.expect(parser)
+    def post(self):
+        args = parser.parse_args()
+        html_data = args["html_content"]
+        duplicate_data = self.html_duplicate_algorithm(html_data)
+        response = [
+            '<button value="Foo" class="aa"/>',
+            '<div><h1 class="red-text"> My Text </h1></div>',
+        ]
+        return {"data": response}, 200
 
 
 if __name__ == "__main__":
